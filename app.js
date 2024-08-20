7a3e511ddffa9e2e33b852ecd5a1adeb8926d497
R0An <roan.ho@ecpay.com.tw>
2024-08-20 17:11:23 +0800

    增加初步  POST 指令

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import crypto from "crypto";
 import cors from "cors";
 const app = express();
 const port = 3000;
+const AESAlgorithm = "aes-128-cbc";
 app.use(express.json());
 app.use(cors());
 
@@ -12,7 +13,7 @@ const MID = {
 };
 
 
-function AESEncrypt() {
+function AESEncrypt(inputParams,HashKey,HashIV) {
   let URLEncoded = encodeURIComponent(inputParams);
   const cipher = crypto.createCipheriv(AESAlgorithm, HashKey, HashIV);
   let EncryptedData = cipher.update(URLEncoded, "utf8", "base64");
@@ -20,12 +21,19 @@ function AESEncrypt() {
   return EncryptedData;
 }
 
-function AESDecrypt() {
+function AESDecrypt(inputParams,HashKey,HashIV) {
   const decipher = crypto.createDecipheriv(AESAlgorithm, HashKey, HashIV);
   let DecryptedData = decipher.update(inputParams, "base64", "utf8");
   DecryptedData += decipher.final("utf8");
   return DecryptedData;
 }
+
+// 取得廠商驗證碼 GetToken(1)：接收前端送來的加密前 Data，並加密
+app.post("/getTokenbyTrade", (req, res) => {
+  const {Data } = req.body;
+  const encryptedData = AESEncrypt(Data);
+  //這裡還沒結束！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
