13d55a1abc9b409e22f70582ec75710d03bf9bf9
evojroan <roan6903@gmail.com>
2024-10-03 14:47:53 +0800

    完成 OrderResultURL

--- app.js
+++ app.js
@@ -2,20 +2,21 @@
 import express from "express";
 import crypto from "crypto";
 import cors from "cors";
-import axios from "axios"; 
+import axios from "axios";
 
 const app = express();
 const port = 3000; //部署到 Vercel 已不需要這行
 const AESAlgorithm = "aes-128-cbc";
+const frontendurl =
+  "https://ecpay-embedded-checkout-git-main-evojroans-projects.vercel.app";
 
-
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors());
 
 const MID = {
-  3002607: { HashKey: "pwFHCqoQZGmho4w6", HashIV: "EkRm7iFT261dpevs" },
-  3003008: { HashKey: "FCnGLNS7P3xQ2q3E", HashIV: "awL5GRWRhyaybq13" },
+  3002607: {HashKey: "pwFHCqoQZGmho4w6", HashIV: "EkRm7iFT261dpevs"},
+  3003008: {HashKey: "FCnGLNS7P3xQ2q3E", HashIV: "awL5GRWRhyaybq13"}
 };
 
 //將 Data 加密
@@ -67,7 +68,7 @@ async function RequestECPayAPIs(action, payload) {
 // 加解密：取得廠商驗證碼 GetTokenbyTrade：接收前端送來的加密前 Data，加密後再呼叫 API (async function RequestECPayAPIs)
 app.post("/GetTokenbyTrade", async (req, res) => {
   try {
-    const { MerchantID, RqHeader, Data } = req.body;
+    const {MerchantID, RqHeader, Data} = req.body;
     const encryptedData = AESEncrypt(
       Data,
       MID[MerchantID].HashKey,
@@ -76,7 +77,7 @@ app.post("/GetTokenbyTrade", async (req, res) => {
     const GetTokenbyTradePayload = {
       MerchantID,
       RqHeader,
-      Data: encryptedData,
+      Data: encryptedData
     };
     const result = await RequestECPayAPIs(
       "GetTokenbyTrade",
@@ -90,14 +91,14 @@ app.post("/GetTokenbyTrade", async (req, res) => {
     res.json(decryptedData.Token);
   } catch (error) {
     console.error("Error in GetTokenbyTrade:", error);
-    res.status(500).json({ error: "內部伺服器錯誤" });
+    res.status(500).json({error: "內部伺服器錯誤"});
   }
 });
 
 // 加解密：建立付款 CreatePayment：接收前端送來的加密前 Data，加密後再呼叫 API (async function RequestECPayAPIs)
 app.post("/CreatePayment", async (req, res) => {
   try {
-    const { MerchantID, RqHeader, Data } = req.body;
+    const {MerchantID, RqHeader, Data} = req.body;
     const encryptedData = AESEncrypt(
       Data,
       MID[MerchantID].HashKey,
@@ -106,7 +107,7 @@ app.post("/CreatePayment", async (req, res) => {
     const CreatePaymentPayload = {
       MerchantID,
       RqHeader,
-      Data: encryptedData,
+      Data: encryptedData
     };
     const result = await RequestECPayAPIs(
       "CreatePayment",
@@ -117,36 +118,56 @@ app.post("/CreatePayment", async (req, res) => {
       MID[MerchantID].HashKey,
       MID[MerchantID].HashIV
     );
-   
+
     res.json(decryptedData);
   } catch (error) {
     console.error("Error in CreatePayment:", error);
-    res.status(500).json({ error: "內部伺服器錯誤" });
+    res.status(500).json({error: "內部伺服器錯誤"});
   }
 });
 
 //解密：接收 OrderResultURL 傳來的加密付款結果通知，解密後再回傳給 OrderResultURL
-//OrderResuktURL 功能尚未完成
+const OrderResult = {};
 app.post("/OrderResultURL", async (req, res) => {
   try {
-    const { MerchantID, Data } = JSON.parse(req.body.ResultData);
+    const {MerchantID, Data} = JSON.parse(req.body.ResultData);
     const decryptedData = AESDecrypt(
       Data,
       MID[MerchantID].HashKey,
       MID[MerchantID].HashIV
     );
     console.log(decryptedData);
-  
+    const MerchantTradeNo = decryptedData.OrderInfo.MerchantTradeNo;
+    console.log("MerchantTradeNo=", MerchantTradeNo);
+    OrderResult[MerchantTradeNo] = decryptedData;
+
+    // 重定向到前端頁面，附帶訂單編號
+    res.redirect(
+      //      `http://localhost:5173/OrderResultURL?MerchantTradeNo=${MerchantTradeNo}`
+      `${frontendurl}/OrderResultURL?MerchantTradeNo=${MerchantTradeNo}`
+    );
   } catch (error) {
     console.error("Error in CreatePayment:", error);
-    res.status(500).json({ error: "OrderResultURL 錯誤" });
+    res.status(500).json({error: "OrderResultURL 錯誤"});
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+// 提供給前端獲取付款結果的 API
+app.get("/api/getOrderResult", (req, res) => {
+  const MerchantTradeNo = req.query.MerchantTradeNo;
+  const OderResultPayload = OrderResult[MerchantTradeNo];
+
+  if (OderResultPayload) {
+    res.json(OderResultPayload);
+  } else {
+    res.status(404).json({error: "找不到付款結果"});
+  }
 });
 
+//部署到 Vercel 取消這段
+// app.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}`);
+// });
 
 // 部署到 Vercel 需要增加這一行
-//export default app;
+export default app;
--- readme.md
+++ readme.md
@@ -1,2 +1,14 @@
-- [ ] 未完成：OrderResultURL
-- [ ] 未完成：ResultURL
+# 站內付 2.0
+
+## 這是什麼？
+
+這是串接綠界金流站內付 2.0 的範例，分別以 React 與 Node.js 實作前後端。
+
+說明文章：
+https://tinyurl.com/3ndpfm27
+
+前端頁面：
+https://ecpay-embedded-checkout-git-main-evojroans-projects.vercel.app/
+
+前端 Repo：
+https://github.com/evojroan/ecpay_embedded_checkout
